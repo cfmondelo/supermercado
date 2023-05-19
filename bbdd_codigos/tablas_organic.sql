6ec99cb1f3f865be90c588a16446582aa6994df6
Andrea <arn@enredada.net>
2023-05-19 18:00:19 +0200

    Base de datos actualizada

--- bbdd_codigos/tablas_organic.sql
+++ bbdd_codigos/tablas_organic.sql
@@ -1,11 +1,10 @@
 create table public.producto (
 	prod_id 	serial  	not null,
 	nombre 		varchar(50) unique not null,
-	precio 		float4 		not null,
-	categoria 	varchar 	not null,
-	fechacad 	date 		null,
-	cantidad 	int 		not null,
 	descripcion varchar(50) not null,
+	categoria 	varchar 	not null,
+	precio 		float4 		not null,
+	cantidad 	int 		not null,
 	imagen 		varchar(50) not null,
     primary key (prod_id)
 );
@@ -14,32 +13,33 @@ create table public.usuarios (
 	nombreusu 	varchar(50) not null,
 	contrasena 	varchar(50) not null,
 	correo 		varchar(50) not null,
+	preg_seg 	varchar(50) not null,
+	resp_seg	varchar(50) not null,
 	direccion 	varchar(50) null,
     primary key (nombreusu)
 );
 
-create table public.cupones (
-	cup_id 		serial 	not null,
-	prod_id 	int 	not null,
-	descuento 	int 	not null,
-	fechaini 	date 	not null,
-	fechafin 	date 	not null,
-	primary key (cup_id),
-    foreign key (prod_id)
-    	references producto (prod_id)
+create table public.descuentos (
+	desc_id 	serial 		not null,
+	código		varchar(15) not null,
+	descuento 	int 		not null,
+	activo 		boolean		not null,
+	primary key (desc_id)
 );
 
 create table public.tickets (
     tick_id 	serial 		not null,
     usuario 	varchar(50) not null,
     prod_id 	int 		not null,
-    cup_id 		int 		not null,
+    desc_id		int 		null,
     precio 		int 		not null,
     cantidad 	int 		not null,
 	fecha	 	date 		not null,
     primary key (tick_id, usuario, prod_id),
+    foreign key (usuario)
+    	references usuarios (nombreusu),
     foreign key (prod_id)
     	references producto (prod_id),
-    foreign key (cup_id)
-    	references cupones (cup_id)
+    foreign key (desc_id)
+    	references descuentos (desc_id)
 );
