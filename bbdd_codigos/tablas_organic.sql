efadebde16747f9bbe97d090128740e65fa44b2c
Andrea <arn@enredada.net>
2023-05-27 18:34:05 +0200

    Añadida tabla carrito

--- bbdd_codigos/tablas_organic.sql
+++ bbdd_codigos/tablas_organic.sql
@@ -1,7 +1,7 @@
 create table public.producto (
 	prod_id 	serial  	not null,
 	nombre 		varchar(50) unique not null,
-	descripcion varchar(50) not null,
+	descripcion varchar(100) not null,
 	categoria 	varchar 	not null,
 	precio 		float4 		not null,
 	cantidad 	int 		not null,
@@ -10,15 +10,15 @@ create table public.producto (
 );
 
 create table public.usuarios (
-	correo 		varchar(50) unique not null,
-	nombre	 	varchar(50) not null,
-	contrasena 	varchar(50) not null,
-	preg_seg 	varchar(50) not null,
-	resp_seg	varchar(50) not null,
-	direccion 	varchar(50) null,
-	cp 			int 		null,
-	ciudad 		varchar(50) null,
-	municipio 	varchar(50) null,
+	correo 		varchar(50) 	unique not null,
+	nombre	 	varchar(50) 	not null,
+	contrasena 	varchar(100) 	not null,
+	preg_seg 	varchar(50) 	not null,
+	resp_seg	varchar(100) 	not null,
+	direccion 	varchar(100) 	null,
+	cp 			int 			null,
+	ciudad 		varchar(50) 	null,
+	municipio 	varchar(50) 	null,
     primary key (correo)
 );
 
@@ -29,6 +29,20 @@ create table public.descuentos (
 	activo 		boolean		not null,
 	primary key (desc_id)
 );
+
+create table public.carrito (
+    carr_id 	serial 		not null,
+    usuario 	varchar(50) not null,
+    prod_id 	int 		not null,
+    precio 		int 		not null,
+    cantidad 	int 		not null,
+    primary key (carr_id, usuario, prod_id),
+    foreign key (usuario)
+    	references usuarios (correo),
+    foreign key (prod_id)
+    	references producto (prod_id)
+);
+
 
 create table public.tickets (
     tick_id 	serial 		not null,
@@ -46,5 +60,47 @@ create table public.tickets (
     foreign key (desc_id)
     	references descuentos (desc_id)
 );
-ALTER TABLE public.usuarios ALTER COLUMN contrasena TYPE varchar(70) USING contrasena::varchar;
-ALTER TABLE public.usuarios ALTER COLUMN resp_seg TYPE varchar(70) USING resp_seg::varchar;
+
+/*
+ * En caso de hacerlo con tres tablas
+ * 
+ create table public.lineapedidos (
+	
+    linea_id 	serial 		not null,
+    usuario 	varchar(50) not null,
+    prod_id 	int 		not null,
+    precio 		int 		not null,
+    cantidad 	int 		not null,
+    primary key (linea_id, usuario, prod_id),
+    foreign key (usuario)
+    	references usuarios (correo),
+    foreign key (prod_id)
+    	references producto (prod_id)
+);
+
+create table public.carrito (
+	usuario 	varchar(50) not null,
+	lineaped	int			not null,
+	primary key (usuario),
+    foreign key (usuario)
+    	references usuarios (correo),
+    foreign key (lineaped)
+    	references lineapedidos (linea_id)
+);
+
+create table public.tickets (
+    tick_id 	serial 		not null,
+    usuario 	varchar(50) not null,
+    lineaped 	int 		not null,
+    desc_id		int 		null,
+    precio 		int 		not null,
+	fecha	 	date 		not null,
+    primary key (tick_id),
+    foreign key (usuario)
+    	references usuarios (correo),
+    foreign key (lineaped)
+    	references lineapedidos (linea_id),
+    foreign key (desc_id)
+    	references descuentos (desc_id)
+);
+*/
